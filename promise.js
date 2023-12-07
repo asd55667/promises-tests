cd5388d1a0e9683cf9faefeffd9d220b5fe19459
wcw <344078971@qq.com>
2023-12-08 00:26:35 +0800

    chore: 2.1

--- promise.js
+++ promise.js
@@ -1,24 +1,35 @@
-class Promise {
-    constructor() {
+
+const PromiseState = {
+    Pending: 0,
+    FullFilled: 1,
+    Rejected: 2
+}
+
+class MyPromise {
+    constructor(state = PromiseState.Pending) {
+        this.state = state
+    }
+
+    then(resolve, reject) {
         // 
     }
 
-
-    static resolve() {
+    resolve() {
         // 
     }
 
-    static reject() {
+    reject() {
         // 
     }
 
-    static resolved() {
-        // 
-    }
-
-    static rejected() {
-        // 
+    static deferred() {
+        const promise = new MyPromise()
+        return {
+            promise,
+            resolve: promise.resolve,
+            reject: promise.reject
+        }
     }
 }
 
-module.exports = Promise
+module.exports = MyPromise
