61031247550dcb3850aaecdc4288beb97f7d6c14
asd55667 <344078971@qq.com>
2023-12-11 13:19:51 +0800

    chore: 2.2.6

--- promise.js
+++ promise.js
@@ -8,29 +8,33 @@ const PromiseState = {
 class MyPromise {
     value
     tasks = []
-    constructor(state = PromiseState.Pending) {
-        this.state = state
+    state = PromiseState.Pending
+    constructor(callback) {
+        if (typeof callback !== 'function')
+            throw new TypeError(`Promise resolver ${typeof callback} is not a function`);
     }
 
     then(resolve, reject) {
-        const task = () => {
-            if (this.state === PromiseState.FullFilled) {
-                try {
-                    resolve?.(this.value)
-                } catch (err) {
-                    this.value = err
-                }
-            } else if (this.state === PromiseState.Rejected) {
-                try {
-                    reject(this.value)
-                } catch (err) {
-                    this.value = err
-                }
+        const p = new MyPromise(() => { })
+
+        const handleCallback = (callback) => {
+            try {
+                p.resolve(callback(this.value))
+            } catch (err) {
+                p.reject(err)
             }
         }
+        const task = () => {
+            if (this.state === PromiseState.FullFilled) {
+                handleCallback((value) => resolve?.(value))
+            } else if (this.state === PromiseState.Rejected) {
+                handleCallback((value) => reject(value))
+            }
+        }
+
         if (this.state === PromiseState.Pending) this.tasks.push(task)
         else setTimeout(task)
-        return this
+        return p
     }
 
     resolve(value) {
@@ -45,11 +49,15 @@ class MyPromise {
         if (this.state !== PromiseState.Pending) return
         this.value = value
         this.state = state
+        this.#clearTask()
+    }
+
+    #clearTask() {
         this.tasks.forEach(task => setTimeout(task))
     }
 
     static deferred() {
-        const promise = new MyPromise()
+        const promise = new MyPromise(() => { })
         return {
             promise,
             resolve: promise.resolve.bind(promise),
